0f8aff6830568e8cba46758985daab62dec9640a
Hadia Khalil <168618032+HadiaKhalil@users.noreply.github.com>
2024-11-22 10:26:12 +0500

    new

--- src/app/veiw/Home/Hero.tsx
+++ src/app/veiw/Home/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
    <div className="  h-full md:w-1/2 w-full max-lg:px-7 max-sm:px-0 ">
    <div className="md:max-w-[70%]  mx-auto md:px-0  md:py-0 max-md:w-[75%] relative">
   
-    <h1 className="lg:text-5xl md:text-3xl  text-2xl  font-bold pt-24 max-w-[440px] max-lg:text-[25px] max-sm:pt-9  ">
+    <h1 className="lg:text-5xl md:text-3xl font-title  text-2xl  font-bold pt-24 max-w-[440px] max-lg:text-[25px] max-sm:pt-9  ">
       I am your <span className="text-blue-800">{`full-stack`}</span>  {`wizard,`} turning <span className="text-blue-800">web dreams</span>  into reality.
      </h1>
      <div className="mt-[-250px] ml-[80px] absolute  max-sm:ml-[10px]  max-sm:mt-[-140px] " >
@@ -26,8 +26,8 @@ const Hero = () => {
        Crafting seamless digital experiences from{` front-end elegance to back-end robustness, `}
        I bring your web visions to life as a versatile  {`full-stack developer`}
          </p>
-         <a className="text-[black]  hover:text-[black] hover:bg-white bg-[#B1C7FF]  flex justify-center transition-all duration-300 ease-in-out items-center
-     max-w-[148px] w-[160px] h-[43px] font-serif px-2 mb-6 pt-[10px] pb-2  rounded">project</a>
+         <a className="text-[black]  hover:text-[black]  hover:bg-white bg-[#B1C7FF]   flex justify-center transition-all duration-300 ease-in-out items-center
+     max-w-[148px] w-[160px] h-[43px]  px-2 mb-6 pt-[10px]  pb-2   rounded">project</a>
     </div>
   </div>
   
