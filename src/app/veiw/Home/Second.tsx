ae4170209c822912325960522a9f741898e2b3ea
Hadia Khalil <168618032+HadiaKhalil@users.noreply.github.com>
2024-11-22 10:00:04 +0500

    new

--- src/app/veiw/Home/Second.tsx
+++ src/app/veiw/Home/Second.tsx
@@ -10,7 +10,7 @@ const Second = () => {
    
       <div className="max-w-[1440px] w-full mx-auto ">
         <div className="flex justify-between items-center md:flex-row flex-col  text-[white]">
-          <div className="  h-full md:w-1/2 w-full max-lg:px-7   max-sm:px-0 max-sm:pr-20">
+          <div className="  h-full md:w-1/2 w-full max-lg:px-7   max-sm:px-0 ">
             <div className="md:max-w-[70%]  mx-auto md:px-0  md:py-0 max-md:w-[75%]  ">
 
               <h1 className="lg:text-5xl md:text-3xl  text-2xl font-extrabold pt-[100px] max-w-[440px] max-lg:text-[25px] max-sm:pt-[100px] ">
