7953dc88b464a5938122c294aad9990c8057f431
Hadia Khalil <168618032+HadiaKhalil@users.noreply.github.com>
2024-11-22 10:03:52 +0500

    new

--- src/app/veiw/Home/Second.tsx
+++ src/app/veiw/Home/Second.tsx
@@ -14,7 +14,7 @@ const Second = () => {
             <div className="md:max-w-[70%]  mx-auto md:px-0  md:py-0 max-md:w-[75%]  ">
 
               <h1 className="lg:text-5xl md:text-3xl  text-2xl font-extrabold pt-[100px] max-w-[440px] max-lg:text-[25px] max-sm:pt-[100px] ">
-                       {` Hello !`}
+                       {` Hello!`}
               </h1>
               <h1 className="lg:text-5xl md:text-3xl  text-2xl  font-extrabold  pt-2 max-w-[440px] max-lg:text-[25px] ">
                 I am <span className="text-blue-800">Laiba Iqbal.
