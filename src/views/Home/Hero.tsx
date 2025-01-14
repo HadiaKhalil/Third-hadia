28a56085df2633beeba4a6f701b0921e54d7fb9d
Hadia Khalil <168618032+HadiaKhalil@users.noreply.github.com>
2025-01-14 11:05:12 +0500

    dd

--- src/views/Home/Hero.tsx
+++ src/views/Home/Hero.tsx
@@ -41,7 +41,7 @@ const Hero = () => {
 
           />
         </div>
-        <div>jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</div>
+      
       </div>
       </div>
     </>
