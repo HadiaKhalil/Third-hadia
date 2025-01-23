0dbe2cd35aca878ba873607174389498a4a4f3c8
Hadia Khalil <168618032+HadiaKhalil@users.noreply.github.com>
2025-01-23 15:37:33 +0500

    ss

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -3,11 +3,13 @@ import Hero from "./Hero";
 import Three from "./Cards"
 import Layout from "@/components/Layout"
 import Pic from "./Pic";
+import Othercards from "./Othercards";
 
 const Home = () => {
   return (
     <Layout>
       <Hero />
+      <Othercards />
       <Pic />
    <Section />
   <Three />
